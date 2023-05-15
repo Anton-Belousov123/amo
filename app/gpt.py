a331ccb3a945662d4b5ef843dc5945fdf6f70e0b
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-15 06:59:27 +0300

    fix docker-compose.yml

--- app/gpt.py
+++ app/gpt.py
@@ -7,15 +7,49 @@ import dotenv
 
 dotenv.load_dotenv('misc/.env')
 
+
+def what_is_the_question(question, m):
+    task = [{'role': 'system', 'content': 'Есть текст. Твоя задача написать цифру, к которой относится данный вопрос.'
+                                          f' Возможные варианты: 0 - {m[0]};'
+                                          f'1 - {m[1]};'
+                                          f'2 - {m[2]};'
+                                          f'3 - {m[3]};'
+                                          f'4 - {m[4]};'
+                                          f'5 - {m[5]};'
+                                          f'6 - {m[6]};'
+             }]
+    task.append({'role': 'user', 'content': question})
+    answer = get_answer(task, 1)
+    while answer != '0' and answer != '1' and answer != '2' and answer != '3' and answer != '4' and answer != '5' and answer != '6':
+        answer = get_answer(task, 1)
+    print('message id: ', answer)
+    return int(answer)
+
+
+def is_answer_correct(question, answer):
+    task = [{'role': 'system', 'content': 'У тебя есть вопрос и ответ. Твоя задача сказать, является этот '
+                                          'ответ корректным на данный вопрос (то есть что ответ совпадает по '
+                                          'смыслу с вопросом). Если ответ корректен - выведи цифру 1, если нет - 0'}]
+    task.append({'role': 'assistant', 'content': "Вопрос: " + question})
+    task.append({'role': 'user', 'content': "Ответ: " + answer})
+    answer = get_answer(task, 1)
+    while answer != '1' or answer != '0':
+        answer = get_answer(task, 1)
+    print('is correct: ', answer)
+    return int(answer)
+
+
 def prepare_request(amo_messages):
     messages = []
-    google_message = sheets.read_message_preview() + ". ОТВЕЧАЙ НА ТОМ ЖЕ ЯЗЫКЕ ЧТО ТЕБЕ ПИШЕТ ПОЛЬЗОВАТЕЛЬ!!!! В твоем сообщении должен быть только ОДИН вопрос." \
-                                                     " Ответ должен быть строго в соответствии с лимитом " \
-                                                     "знаков который я указал. Ты не должен отправлять никакие свои варианты клиенту." \
-                                                     "Действуй строго в последовательности что я указал. Если ты не получил корректный ответ на вопрос, то задай его снова." \
-                                                     "Если ты уже задал все вопросы, то ответь спасибо, ожидайте ответа." \
-                                                     "Не говори никакую лишнюю информацию не входящую в сценарий!"
-    text_length = len(google_message)
+    print(amo_messages)
+    rules, length, messages = sheets.read_message_preview()
+    index = what_is_the_question(amo_messages[1], messages)
+    status = is_answer_correct(amo_messages[1], amo_messages[0])
+    text_length = len(rules)
+    if status == 0 or index + 1 >= len(messages):
+        messages.append({'role': 'system', 'content': messages[index]})
+    else:
+        messages.append({'role': 'system', 'content': messages[index + 1]})
     for amo_message in amo_messages:
         if text_length + len(amo_message['text']) > 4000:
             break
@@ -24,7 +58,8 @@ def prepare_request(amo_messages):
             messages.append({"role": "assistant", "content": amo_message['text']})
         else:
             messages.append({"role": "user", "content": amo_message['text']})
-    messages.append({'role': 'system', 'content': google_message})
+    messages.append({'role': 'system', 'content': rules})
+
     response = []
     for i in messages:
         if i['content'] == '/restart':
@@ -32,22 +67,25 @@ def prepare_request(amo_messages):
         response.append(i)
     if len(response) == 0:
         response.append({"role": "user", "content": "Привет. Я новый клиент. Разговаривай со мной на моем языке"})
-        response.append({'role': 'system', 'content': google_message})
+        response.append({'role': 'system', 'content': rules})
+        response.append({'role': 'system', 'content': messages[0]})
     response.reverse()
     print(response)
     return response
 
 
-def get_answer(messages: list):
+def get_answer(messages: list, limit):
     print(messages)
     try:
-    #if True:
+        # if True:
         openai.api_key = os.getenv('CHAT_GPT_KEY')
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=messages
+            messages=messages,
+            max_tokens=int(limit)
         )
         print(response, 'response')
+
         return response['choices'][0]['message']['content']
 
     except Exception as e:
--- app/sheets.py
+++ app/sheets.py
@@ -8,5 +8,14 @@ def read_message_preview() -> str:
     creds = ServiceAccountCredentials.from_json_keyfile_name('misc/chat-384709-f24e97dc0a0b.json', scope)
     client = gspread.authorize(creds)
     sheet = client.open('ЧАТ').sheet1
-    value = sheet.cell(1, 1).value
-    return value
+    rules = sheet.cell(1, 1).value
+    length = sheet.cell(2, 1).value
+    messages = [
+        sheet.cell(3, 1).value,
+        sheet.cell(4, 1).value,
+        sheet.cell(5, 1).value,
+        sheet.cell(6, 1).value,
+        sheet.cell(7, 1).value,
+        sheet.cell(8, 1).value,
+                ]
+    return rules, length, messages
